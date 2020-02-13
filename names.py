fe8dcf8217fbd5464cb87871d67ebe2c46080cc2
clone2020 <60959693+clone2020@users.noreply.github.com>
2020-02-13 07:33:18 +0530

    Create names.py
    Names app v1

---
+++ names.py
@@ -0,0 +1,11 @@
+#!/use/bin/python
+
+def checkName(name):
+    checkName = input("Is your name, " + name + " ? ")
+
+        if checkName.lower() == "yes":
+            print("hello ,", name)
+        else:
+            print("we are sorry! ,", name)
+
+checkName(rudra)
