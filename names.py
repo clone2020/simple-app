bef44a7df4b7ef186a842eb7a0b17671b1849577
clone2020 <60959693+clone2020@users.noreply.github.com>
2020-02-13 09:31:31 +0530

    Indentation and quotes
    Update file for syntax error

--- names.py
+++ names.py
@@ -3,9 +3,9 @@
 def checkName(name):
     checkName = input("Is your name, " + name + " ? ")
 
-        if checkName.lower() == "yes":
-            print("hello ,", name)
-        else:
-            print("we are sorry! ,", name)
+    if checkName.lower() == "yes":
+        print("hello ,", name)
+    else:
+        print("we are sorry! ,", name)
 
-checkName(rudra)
+checkName("rudra")
